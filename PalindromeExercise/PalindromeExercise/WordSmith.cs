2d642d8914a627e4fc26be7e2c0214a299df739b
sreynolds09 <sara.reynolds.clan@gmail.com>
2025-06-23 17:30:42 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    
+    public bool IsAPalindrome(string word)
+    {
+        var reversed = new string(word.ToLower().Reverse().ToArray());
+        return word.ToLower() == reversed;
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+            [InlineData("civic", true)]
+            [InlineData("RaceCar", true)]
+            [InlineData("hello", false)]
+            [InlineData("kayak", true)]
+            [InlineData("noon", true)]
+            [InlineData("palindrome", false)]
+        public void Test1(string input, bool expected)
         {
+                // Arrange
+                var wordSmith = new WordSmith();
+
+                // Act
+                var actual = wordSmith.IsAPalindrome(input);
+
+                // Assert
+                Assert.Equal(expected, actual);
 
         }
     }
